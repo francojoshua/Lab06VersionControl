d753e04605e0031df85fab05a951a4ca5eceb50e
Joshua <joshuafrancostudent@gmail.com>
2023-10-24 14:12:26 -0400

    add main file w/ encoder function

---
+++ main.py
@@ -0,0 +1,41 @@
+def encode(string: str):
+    encoded_pass = ""
+
+    for n in list(string):
+        value = str(int(n) + 3)
+        encoded_pass += (value if len(value) == 1 else value[-1])
+
+    return encoded_pass
+
+
+def decode(string: str):
+    pass
+
+
+def main():
+    password = ""
+
+    while True:
+        print("Menu")
+        print("-------------")
+        print("\n".join(["1. Encode", "2. Decode", "3. Quit"]))
+
+        option = input("\nPlease enter an option: ")
+
+        if option == "1":
+            decoded_pass = input("Please enter your password to encode: ")
+            print("Your password has been encoded and stored!")
+
+            password = encode(decoded_pass)
+        elif option == "2":
+            print(f"The encoded password is {password}, and the original password is {decode(password)}.")
+        elif option == "3":
+            break
+        else:
+            print("Invalid input")
+
+        print()
+
+
+if __name__ == "__main__":
+    main()
